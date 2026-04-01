5c44a8fbbd902cd37bc37ac62736253ca7eddadd
Anshraj11111 <162826050+Anshraj22@users.noreply.github.com>
2026-04-01 12:16:42 +0530

    update countdown to 9th april 2026

--- src/pages/home/components/NewHeroSection.jsx
+++ src/pages/home/components/NewHeroSection.jsx
@@ -13,7 +13,7 @@ const NewHeroSection = () => {
   });
 
   useEffect(() => {
-    const targetDate = new Date('2026-04-08T00:00:00').getTime();
+    const targetDate = new Date('2026-04-09T00:00:00').getTime();
     const timer = setInterval(() => {
       const now = new Date().getTime();
       const difference = targetDate - now;
